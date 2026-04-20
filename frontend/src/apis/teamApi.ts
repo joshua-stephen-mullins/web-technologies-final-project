12e572c76eb1f4c2884b2b15e2820700cf8104d6
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2026-04-20 16:06:01 -0500

    Updated files to test azure deployment

--- frontend/src/apis/teamApi.ts
+++ frontend/src/apis/teamApi.ts
@@ -1,5 +1,3 @@
-import axiosClient from './axiosClient'
-
 // Owner: Oscar (Person 2) — UC-7,8,9,10,12,13,14
 export const teamApi = {
   // TODO: search(params), getById(id), create(data), update(id, data), remove(id),
